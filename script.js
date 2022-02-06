721c6fe623b70966660c14ec5b9da088cd51caf5
Thays Omine <thaysomine@gmail.com>
2022-02-06 05:53:28 -0300

    função para clicar nas cartas implementada

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-// criar função para perguntar com quantas cartas ira jogar
-let nOfCards = null;  //num de cartas a ser escolhido pelo usuario
-const cards = document.querySelectorAll('.card');
+let nOfCards = null;  // num de cartas a ser escolhido pelo usuario
+let cards = document.querySelectorAll('.card');
 let array = [];
 const deck = document.querySelector(".deck");
+
 // função para pedir num de cartas
 function cardNum() {
     while (nOfCards % 2 !== 0 || nOfCards < 4 || nOfCards > 14) {
@@ -12,27 +12,23 @@ function cardNum() {
     return(nOfCards);
 }
 cardNum();
-//função para mostrar numero de cartas pedido
+
+// função para mostrar numero de cartas pedido
 function showCards() {
     for (let i=0 ; i<nOfCards ; i++) {
         const show = cards[i];
         array.push(show);
         show.classList.remove("hidden");
-        show.classList.add("flip");
     }
 array.sort(comparador);  
 }
-//funcao p embaralhar 
 
-
-// Esta função pode ficar separada do código acima, onde você preferir
+// funcao p embaralhar as cartas
 function comparador() { 
 	return Math.random() - 0.5; 
 }
 
-
-//função para virar as cartas
-//cards.forEach(card)
+// função p mandar a array embaralhada antes do conteudo carregar
 function startGame() {
     for (let i= 0; i < array.length; i++){
     [].forEach.call(array, function(item){
@@ -40,5 +36,10 @@ function startGame() {
     });
     }
 }
-
 window.onload = startGame();
+
+//função para virar as cartas
+function flipCard(element) {
+    element.querySelector(".front-face").classList.toggle("flip");
+    element.querySelector(".back-face").classList.toggle("flip"); 
+}
